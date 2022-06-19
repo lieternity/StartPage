bab1e2454509971d888da67094272b586d0e1b0f
Admin <lieternidad@gmail.com>
2022-06-20 01:25:42 +0800

    包子起始页

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    pwa: {
+        name: 'baoZiStartPage',
+        themeColor: '#ffffff',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        assetsVersion:"0.1",
+
+        // configure the workbox plugin
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            // swSrc: 'registerServiceWorker.js',
+            // ...other Workbox options...
+        }
+    }
 })
