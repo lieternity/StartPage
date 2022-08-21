d4b2f74f2fbc8b3da20992593b6051c49946ec1c
Admin <lieternidad@gmail.com>
2022-08-21 08:56:11 +0800

    包子起始页

---
+++ src/elementui/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import {
+    Select,
+    Option,
+    Collapse,
+    Col,
+    collapseItem,
+    Button,
+    Input,
+    Switch,
+    Popover,
+    Card,
+    Avatar,
+    FormItem,
+    Form,
+    Upload,
+    Timeline,
+    TimelineItem,
+    Dialog,
+    Autocomplete,
+    Backtop,
+    Divider,
+    Slider,
+    MessageBox,
+    Message
+} from 'element-ui';
+
+Vue.use(Card)
+Vue.use(Divider)
+Vue.use(Slider)
+Vue.use(Backtop)
+Vue.use(Col)
+Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(Dialog)
+Vue.use(TimelineItem)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Avatar)
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Collapse);
+Vue.use(Switch);
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Popover);
+Vue.use(collapseItem);
+Vue.use(Autocomplete)
+
+
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$message = Message
