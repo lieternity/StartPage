8a3ac7599dbc77957d2259679f65d8a2d6111f18
Admin <lieternidad@gmail.com>
2022-09-03 11:47:42 +0800

    包子起始页

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,46 +1,53 @@
 import {
-  register
+    register
 } from 'register-service-worker'
 import {
-  Message,
-  MessageBox
+    Message,
+    MessageBox
 } from 'element-ui'
+
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    registered() {
-      console.log('Service worker has been registered.')
-    },
-    cached() {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound() {
-      Message({
-        message: '检测到新版本，正在下载',
-        type: 'info'
-      })
-      console.log('New content is downloading.')
-    },
-    updated() {
-      MessageBox({
-        title: '更新',
-        message: '新版本内容下载完成，点击确定按钮立即使用新版',
-        confirmButtonText: '确定',
-        showClose: false,
-        showCancelButton: false,
-        closeOnClickModal: false,
-        closeOnPressEscape: false,
-        type: 'success',
-        callback() {
-          location.reload()
+    console.log(process.env.NODE_ENV)
+    register(`${process.env.BASE_URL}service-worker.js`, {
+        registered() {
+            console.log('Service worker has been registered.')
+        },
+        cached() {
+            console.log('Content has been cached for offline use.')
+        },
+        updatefound() {
+            if (localStorage.getItem("firstTime") !== "true") {
+                return
+            } else {
+                localStorage.setItem("firstTime", "true")
+            }
+            Message({
+                message: '检测到新版本，正在下载',
+                type: 'info'
+            })
+            console.log('New content is downloading.')
+        },
+        updated() {
+            MessageBox({
+                title: '更新',
+                message: '新版本内容下载完成，点击确定按钮立即使用新版',
+                confirmButtonText: '确定',
+                showClose: false,
+                showCancelButton: false,
+                closeOnClickModal: false,
+                closeOnPressEscape: false,
+                type: 'success',
+                callback() {
+                    location.reload()
+                }
+            })
+            console.log('New content is available; please refresh.')
+        },
+        offline() {
+            console.log('No internet connection found. App is running in offline mode.')
+        },
+        error(error) {
+            console.error('Error during service worker registration:', error)
         }
-      })
-      console.log('New content is available; please refresh.')
-    },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error(error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+    })
 }
